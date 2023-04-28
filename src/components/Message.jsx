8ffec5817d2434ec6447ae305c4e464f86c2ba70
AKW <2497744746@qq.com>
2023-04-28 11:33:33 +0800

    finish design

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import girl from '../img/girl.jpg'
+import avatar from '../img/avatar.jpg'
 
 const Message = () => {
     return (
-        <div>Message</div>
+        <div className='message owner'>
+            <div className="messageInfo">
+                <img src={girl} alt="" />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+                <img src={avatar} alt="" />
+            </div>
+        </div>
     )
 }
 
